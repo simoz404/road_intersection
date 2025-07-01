c1f0517f2dd8056ecc594e0bdd4b629dfcb35d34
mbakhouc <bakhouch2019@gmail.com>
2025-07-01 16:31:40 +0100

    adding lines of roads

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+// use sdl2::pixels::Color;
+
+// enum Dirr {
+//     ShortTurn,
+//     LongTurn,
+//     Direct,
+// }
+// pub struct Car {
+//     x: i32,
+//     y: i32,
+//     color: Color,
+//     start: String,
+//     dir: Dirr,
+// }
+
+// const speed:i32 = 5;
+
+// impl  Car {
+//     fn new(x: i32, y:i32, color:Color, start: String) -> Self {
+//         Car { x, y, color, start }
+//     }
+
+//     fn draw_car(&self) {
+        
+//     }
+//     fn move_car(&mut self, dir:&str) {
+//         match dir {
+//             "UP" => {},
+//             "DOWN" => {},
+//             "LEFT" => {},
+//             "RIGHT" =>{}
+//         }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,43 @@ pub fn main() {
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
+    
+
+    
     'running: loop {
+
+        
+        canvas.set_draw_color(Color::YELLOW);
+        canvas.draw_point(Point::new(25, 325));
+        canvas.draw_point(Point::new(425, 575));
+        canvas.draw_point(Point::new(375, 25));
+        canvas.draw_point(Point::new(775, 275));
+
         canvas.set_draw_color(Color::WHITE);
         let _ = canvas.draw_line(Point::new(1, 250), Point::new(800, 250));
         let _ = canvas.draw_line(Point::new(1, 350), Point::new(800, 350));
+        let _ = canvas.draw_line(Point::new(1, 300), Point::new(800, 300));
         let _ = canvas.draw_line(Point::new(350, 1), Point::new(350, 600));
         let _ = canvas.draw_line(Point::new(450, 1), Point::new(450, 600));
+        let _ = canvas.draw_line(Point::new(400, 1), Point::new(400, 600));
+
+        canvas.set_draw_color(Color::YELLOW);
+        canvas.draw_point(Point::new(450, 350));
+        canvas.draw_point(Point::new(500, 400));
+
+        canvas.draw_point(Point::new(300, 400));
+        canvas.draw_point(Point::new(350, 350));
+
+
+
+
         for event in event_pump.poll_iter() {
             match event {
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {},
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {println!("down"); },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {println!("down"); },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {println!("down"); },
+
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
@@ -42,3 +71,8 @@ pub fn main() {
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
+
+/* fn create_up(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+    canvas.set_draw_color(Color::WHITE);
+    let _ = canvas.draw_rect();
+} */
